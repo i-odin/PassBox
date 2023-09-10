fa3d0b4e7726d1f91ad6b434601abe13909981d9
Илья <ily-odinokov@yandex.ru>
2023-09-10 16:20:12 +0300

    fix expand

--- src/PassBox.Mobile/ViewModels/SiteViewModel.cs
+++ src/PassBox.Mobile/ViewModels/SiteViewModel.cs
@@ -1,27 +1,27 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using PassBox.Mobile.Models;
 using PassBox.Mobile.ViewModels.Base;
 using PassBox.Mobile.Views;
+using System.Collections.ObjectModel;
 
 namespace PassBox.Mobile.ViewModels;
 
 public partial class SiteViewModel : BaseViewModel
 {
-    public IEnumerable<Site> Sites
+    public SiteViewModel() 
     {
-        get
-        {
-            return new List<Site> { new Site { Id = Guid.NewGuid(), Name = "Google", Address = "google.com" }, new Site { Id = Guid.NewGuid(), Name = "Yandex", Address = "yandex.ru" }, new Site { Id = Guid.NewGuid(), Name = "Vk", Address = "vk.ru" } };
-        }
+        Sites = new ObservableCollection<Site> { new Site { Id = Guid.NewGuid(), Name = "Google", Address = "google.com" }, new Site { Id = Guid.NewGuid(), Name = "Yandex", Address = "yandex.ru" }, new Site { Id = Guid.NewGuid(), Name = "Vk", Address = "vk.ru" } };
+        accounts = new List<Account>();
     }
+    public ObservableCollection<Site> Sites { get; set; }
 
-    public IEnumerable<Account> Accounts { get; set; }
-    /*{
-        get
-        {
-            return new List<Account> { new Account { Name = "Логин", Password = "фывлт2ш315тр198нат9фн1" }, new Account { Name = "фрпфыыджпфжд", Password = "фылафлыт 3215735", Description = "aadngn35" } };
-        }
-    }*/
+    [ObservableProperty]
+    private IEnumerable<Account> accounts;
+
+    //[ObservableProperty]
+    //private bool isExpanded;
 
     [RelayCommand]
     public async void Edit()
@@ -32,6 +32,8 @@ public partial class SiteViewModel : BaseViewModel
     [RelayCommand]
     public void Print(Guid id)
     {
+        //var qweq = IsExpanded;
+        //Accounts.Add(new Account { Name = "Логин", Password = "фывлт2ш315тр198нат9фн1" });
         Accounts = new List<Account> { new Account { Name = "Логин", Password = "фывлт2ш315тр198нат9фн1" }, new Account { Name = "фрпфыыджпфжд", Password = "фылафлыт 3215735", Description = "aadngn35" } };
     }
 
